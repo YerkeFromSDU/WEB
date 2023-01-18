1a3d320b78b38cce8d16eac311b851facca3cce8
YerkeFromSDU <78737964+YerkeFromSDU@users.noreply.github.com>
2023-01-18 18:00:35 +0500

    Add files via upload

---
+++ news-homepage-main/main.js
@@ -0,0 +1,26 @@
+// var mobileNav = document.querySelector(".nav");
+// var mobileBtn = document.querySelector(".icon");
+
+// mobileBtn.addEventListener("click", () => {
+//   if (mobileNav.style.display === "none") {
+//     mobileNav.style.display = "flex";
+//   } 
+//   else {
+//     mobileNav.style.display = "none";
+//   }
+// });
+
+  
+
+function myFunction() {
+    var x = document.getElementById("myLink");
+    
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+
+
+
